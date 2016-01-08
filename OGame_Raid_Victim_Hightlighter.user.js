68ff39e09a5ac877bc4d629577976df50136e9eb
Sören Grzanna <soeren.grzanna@stud.fh-hannover.de>
2016-01-08 02:05:59 +0100

    Initial commit

---
+++ OGame_Raid_Victim_Hightlighter.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name        OGame Raid Victim Hightlighter
+// @namespace   de.grzanna-online.ogame
+// @include     http://*.ogame.gameforge.com/game/index.php?page=galaxy*
+// @version     1
+// @grant       none
+// ==/UserScript==
